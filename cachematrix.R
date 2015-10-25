acde352b63329d1dd2b698bff3bc978916c5917d
velu18 <velubee@gmail.com>
2015-10-25 14:25:45 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -25,7 +25,7 @@ makeCacheMatrix <- function(x = matrix())
 # The following function calculates the inverse of the matrix created with the above function.
 # However, it first checks to see if the inverse has already been calculated.
 # If so, it gets the inverse from the cache and skips the computation.
-# Otherwise, it calculates the inverse of the matrix and sets the value of the mean in the cache via the setinverse function.
+# Otherwise, it calculates the inverse of the matrix and sets the inverse of the matrix in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...)
 {
